f3d7099ee331402b3c9092af69095253e872cd85
arfyha <arne.fynn.hass@gmail.com>
2025-04-12 13:08:59 +0000

    cleanup

--- src/draw/src/draw_circle_whiteboard.cpp
+++ src/draw/src/draw_circle_whiteboard.cpp
@@ -35,31 +35,12 @@ public:
 
     visual_tools_->deleteAllMarkers();
     visual_tools_->loadRemoteControl();
-
-    logBasicInfo();
   }
 
 private:
   std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group_interface_;
   rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr drawing_positions_sub_;
   std::shared_ptr<moveit_visual_tools::MoveItVisualTools> visual_tools_;
-  moveit_msgs::msg::Constraints orientation_constraints_;
-  geometry_msgs::msg::Quaternion q_msg_;
-
-  const double radius_ = 0.2;
-  const double center_x_ = 0.6;
-  const double center_y_ = -0.17;
-  const double center_z_ = 0.75;
-  const int num_points_ = 90;
-
-  void logBasicInfo() {
-    RCLCPP_INFO(this->get_logger(), "Planning frame: %s", move_group_interface_->getPlanningFrame().c_str());
-    RCLCPP_INFO(this->get_logger(), "End effector link: %s", move_group_interface_->getEndEffectorLink().c_str());
-    RCLCPP_INFO(this->get_logger(), "Available Planning Groups:");
-    for (const auto &group : move_group_interface_->getJointModelGroupNames()) {
-      std::cout << "  - " << group << std::endl;
-    }
-  }
 
   void drawTitle(const std::string &text) {
     Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
@@ -71,9 +52,6 @@ private:
   void drawCircle(const geometry_msgs::msg::PoseArray &posearray) {
     std::vector<geometry_msgs::msg::Pose> waypoints;
     for (size_t i = 0; i < posearray.poses.size(); ++i) {
-        RCLCPP_INFO(this->get_logger(), "Pose added position: x:%f y:%f z:%f \n orientation: x:%f y:%f z:%f w:%f",
-            posearray.poses[i].position.x, posearray.poses[i].position.y, posearray.poses[i].position.z,
-            posearray.poses[i].orientation.x, posearray.poses[i].orientation.y, posearray.poses[i].orientation.z, posearray.poses[i].orientation.w);
         auto pose = posearray.poses.at(i);
         waypoints.push_back(pose);
     }
@@ -115,6 +93,9 @@ private:
         move_group_interface_->execute(plan);
 
         RCLCPP_INFO(this->get_logger(), "Circle drawn successfully.");
+
+        rclcpp::shutdown();
+        return;
     } else {
         RCLCPP_ERROR(this->get_logger(), "Failed to plan Cartesian path with sufficient coverage after %i attempts.", attempt);
     }
